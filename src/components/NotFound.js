fa328659baa16f1707553b44b5440a726e098358
alireza-hp elitedesk pc <alireza.zabihi@yahoo.com>
2022-11-29 21:34:41 +0330

    temporary fix for initial load of wrong route

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,20 +1,20 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import "./styles/NotFound.css";
 
 function NotFound() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    setTimeout(() => {
-      navigate("/");
-    }, 3000);
-  }, []);
-
-  return (
-    <div className="not-found">
-      <p>Invalid URL</p>
-    </div>
-  );
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     navigate("/");
+  //   }, 3000);
+  // }, []);
+  // return (
+  //   <div className="not-found">
+  //     <p>Invalid URL</p>
+  //   </div>
+  // );
+  return <Navigate to={"/"}></Navigate>;
 }
 
 export default NotFound;
